6a076a262d1636ab381dcf8eeb8defe4358d41cc
snowboredagain <56096344+snowboredagain@users.noreply.github.com>
2026-08-19 16:27:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -188,7 +188,7 @@ app.get('/api/customer/verify', requireApiKey, async (req, res) => {
   return res.json({
     verification: [ {
       success:     true,
-      customerId:  `${data.customerId}.00`,    // "1865.00" - define as a number
+      customerId:  parseFloat(data.customerId),    // define as a number
       companyName: data.companyName,
       hasTerms:    data.hasTerms,
       requiresPO:  data.requiresPO,
