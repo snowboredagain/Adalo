81d48ee60003e3d47c4c52928382a3ad9d719fb1
snowboredagain <56096344+snowboredagain@users.noreply.github.com>
2026-08-19 15:50:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -188,7 +188,7 @@ app.get('/api/customer/verify', requireApiKey, async (req, res) => {
   return res.json({
     verification: [ {
       success:     true,
-      customerId:  'NSID${data.customerId}',    // "NSID1865" - clearly not a date
+      customerId:  `NSID${data.customerId}`,    // "NSID1865" - clearly not a date
       companyName: data.companyName,
       hasTerms:    data.hasTerms,
       requiresPO:  data.requiresPO,
